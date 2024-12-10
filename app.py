8a9267822b6324443c17cb55fb70dc40ddc86552
EmCab <152331204+emem83@users.noreply.github.com>
2024-12-10 15:05:08 -0500

    imported sklearn

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import pandas as pd
 #import matplotlib.pyplot as plt
 #import seaborn as sns
 import numpy as np
+import sklearn
+from sklearn import preprocessing
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
